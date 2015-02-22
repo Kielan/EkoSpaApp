c705bf7d61e13886aa9b075f61bd79a33e2cae21
Kielan Lemons <kielan@Kielans-MacBook-Pro.local>
2015-02-22 13:09:40 -0800

    moved persisting data from controller to factory service

--- js/controllers.js
+++ js/controllers.js
@@ -6,56 +6,16 @@ ctrls.controller('AppCtrl', function ($scope, $http) {
 	$scope.date = new Date();
 
 });
-ctrls.controller('HomeCtrl', function ($scope, $http) {
+ctrls.controller('HomeCtrl', function ($scope, $http, ) {
 	$scope.tagline = "this is an awesome HOME tagline";
 	$scope.title = "HOME";
-	$scope.developer = {
-    name: 'Paige',
-    handle: 'paigetech'
-	};
-    $scope.kendamas = [
-	{
-	    "title": "Wacky Face",
-	    "img": "kendamas/k1.jpg",
-	    "size": "275px",
-	    "tags": "#kcs #wood #plain #smiley #face #light #more #tags #kendama"
-	    
-	},
-	{
-	    "title": "Kaleb Purple Cracked",
-	    "img": "kendamas/k2.jpg",
-	    "size": "275px",
-	    "tags": "#cracked #kaleb #purple #awesome #kendama"
-	},
-	{
-	    "title": "Oak Wood",
-	    "img": "kendamas/k3.jpg",
-	    "size": "275px",
-	    "tags": "#burnt #stripe #kcs #kendama #nice #pro"
-	},
-	{
-	    "title": "Trojax Kendama",
-	    "img": "kendamas/k4.jpg",
-	    "size": "275px",
-	    "tags": "#cracked  #green #blue  #teal #balsa #bahama  #kendama #spike"
-	},
-	{
-	    "title": "Vintage Ako Camo",
-	    "img": "kendamas/k5.jpg",
-	    "size": "275px",
-	    "tags": "#camoflauge #blue #hand #painted #ako #kendama #camo #black #blackstring"
-	},
-	{
-	    "title": "Coral Reef",
-	    "img": "kendamas/k6.jpg",
-	    "size": "275px",
-	    "tags": "#wild #sweets #kendama #blue #black #coating #old"
-	}	    
-    ];
+    $scope.
 
 });
 
-ctrls.controller('AboutCtrl', function ($scope, $http) {
+ctrls.controller('AboutCtrl', function ($scope, $http, Kenbasket) {
 	$scope.tagline = "this is an awesome ABOUT tagline";
-	$scope.title = "ABOUT";
+    $scope.title = "ABOUT";
+    $scope.newKen = {};
+    $scope.kenBasket = kenBasket;
 });
